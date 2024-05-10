0af8cde4ae2274053e119ada87204e9a84dcb611
limeng <13020102221@163.com>
2024-05-10 21:19:53 +0800

    update partner api

--- src/pages/api/partner.js
+++ src/pages/api/partner.js
@@ -5,25 +5,95 @@ export default async function handler(req, res) {
     success: true,
     data: [
       {
-        "no": 1,
-        "name": "柠檬草",
-        "img": "/scents/ningmengcaoheshuweicao.png",
+        "no": 345,
+        "name": "印记密码",
+        "img": "",
         "color": "#A9DAD7",
-        "period": 73500
+        "period": 123000
       },
       {
-        "no": 2,
-        "name": "小山坡",
+        "no": 348,
+        "name": "大王来巡山",
         "img": "/scents/xiaoshanpo.png",
-        "color": "#C2E593",
-        "period": 72000
+        "color": "#A9DAD7",
+        "period": 288000
       },
       {
-        "no": 3,
-        "name": "柠檬",
-        "img": "/scents/ningmeng.png",
-        "color": "#C2E593",
-        "period": 0
+        "no": 561,
+        "name": "摇曳纯净",
+        "img": "/scents/lanfengling.png",
+        "color": "#A9DAD7",
+        "period": 87000
+      },
+      {
+        "no": 655,
+        "name": "加州阳光",
+        "img": "/scents/chenlu.png",
+        "color": "#A9DAD7",
+        "period": 180000
+      },
+      {
+        "no": 665,
+        "name": "精神满满",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 126000
+      },
+      {
+        "no": 700,
+        "name": "枕边蜜语",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 105000
+      },
+      {
+        "no": 702,
+        "name": "心动一夏",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 120000
+      },
+      {
+        "no": 1529,
+        "name": "醉梦清欢",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 180000
+      }
+      {
+        "no": 1530,
+        "name": "茉香旅途",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 225000
+      }
+      {
+        "no": 1531,
+        "name": "薰衣仲夜",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 180000
+      }
+      {
+        "no": 1532,
+        "name": "小曹寻味",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 120000
+      }
+      {
+        "no": 1533,
+        "name": "晨曦薄荷",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 180000
+      }
+      {
+        "no": 1534,
+        "name": "西湖桂花",
+        "img": "",
+        "color": "#A9DAD7",
+        "period": 225000
       }
     ]
   })
