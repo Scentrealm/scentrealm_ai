3524fb8477f0b0f16ef9c981e1209bb3eb2c38b7
limeng <13020102221@163.com>
2024-09-10 14:58:43 +0800

    update xiaobo api

--- src/pages/api/scenic.js
+++ src/pages/api/scenic.js
@@ -3,6 +3,9 @@ export default async function handler(req, res) {
     code: 200,
     msg: 'success',
     success: true,
-    data: [10115, 10096, 10078, 10075, 10074, 10073, 10061, 10060, 10143, 10127, 10125, 10123, 10098, 10055]
+    data: {
+      img: 'https://scenic-qiweiwangguo-com.oss-cn-hangzhou.aliyuncs.com/uploads/web/image/1725951374254-WechatIMG12.jpg',
+      device: [10115, 10096, 10078, 10075, 10074, 10073, 10061, 10060, 10143, 10127, 10125, 10123, 10098, 10055]
+    }
   })
 }
