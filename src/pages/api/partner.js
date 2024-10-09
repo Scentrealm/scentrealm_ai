d5ead168f0e25832e32f6d03d2dacd9c94d4126e
limeng <13020102221@163.com>
2024-10-09 18:32:30 +0800

    update sleep api

--- src/pages/api/partner.js
+++ src/pages/api/partner.js
@@ -4,10 +4,10 @@ export default async function handler(req, res) {
     msg: 'success',
     success: true,
     page: {
-      total: 13,
-      current_page: 1,
+      n: 2000,
+      total: 34,
       last_page: 1,
-      n: 2000
+      current_page: 1,
     },
     data: [
       {
@@ -100,6 +100,132 @@ export default async function handler(req, res) {
         "img": "",
         "color": "#A9DAD7",
         "period": 225000
+      },
+      {
+        "no": 231,
+        "name": "太阳的爱",
+        "img": "/scents/chenlu.png",
+        "period": 87000
+      },
+      {
+        "no": 876,
+        "name": "蒜蓉小龙虾",
+        "img": "",
+        "period": 60000
+      },
+      {
+        "no": 232,
+        "name": "百万美元",
+        "img": "",
+        "period": 123000
+      },
+      {
+        "no": 700,
+        "name": "枕边蜜语",
+        "img": "/scents/miju.png",
+        "period": 105000
+      },
+      {
+        "no": 702,
+        "name": "心动一夏",
+        "img": "/scents/marry me.png",
+        "period": 120000
+      },
+      {
+        "no": 665,
+        "name": "精神满满",
+        "img": "/scents/bohe.png",
+        "period": 126000
+      },
+      {
+        "no": 1819,
+        "name": "桂在有你",
+        "img": "",
+        "period": 288000
+      },
+      {
+        "no": 1820,
+        "name": "无人区玫瑰",
+        "img": "",
+        "period": 120000
+      },
+      {
+        "no": 747,
+        "name": "小雏菊",
+        "img": "",
+        "period": 87000
+      },
+      {
+        "no": 1821,
+        "name": "佛曰无所谓",
+        "img": "",
+        "period": 99000
+      },
+      {
+        "no": 34,
+        "name": "檀香",
+        "img": "/scents/tanxiang.png",
+        "period": 120000
+      },
+      {
+        "no": 577,
+        "name": "沉香",
+        "img": "/scents/chenxiang.png",
+        "period": 135000
+      },
+      {
+        "no": 1822,
+        "name": "Cool Water",
+        "img": "",
+        "period": 288000
+      },
+      {
+        "no": 1823,
+        "name": "柠檬与鼠尾",
+        "img": "",
+        "period": 453600
+      },
+      {
+        "no": 762,
+        "name": "小山坡",
+        "img": "",
+        "period": 123000
+      },
+      {
+        "no": 1824,
+        "name": "浆果汽水儿",
+        "img": "",
+        "period": 360000
+      },
+      {
+        "no": 191,
+        "name": "大地",
+        "img": "",
+        "period": 123000
+      },
+      {
+        "no": 56,
+        "name": "海洋",
+        "img": "",
+        "period": 123000
+      },
+      {
+        "no": 5,
+        "name": "森林",
+        "img": "/scents/senlin.png",
+        "period": 105000
+      },
+      {
+        "no": 1826,
+        "name": "心想事橙",
+        "img": "",
+        "period": 378000
+      },
+      {
+        "no": 1825,
+        "name": "麻辣鲜香",
+        "img": "",
+        "period": 60000
       }
     ]
   })
