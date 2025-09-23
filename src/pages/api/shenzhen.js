62eca1310cda2e0715743cce21f7b55cf140b1be
limeng <13020102221@163.com>
2025-09-23 11:18:57 +0800

    update config

---
+++ src/pages/api/shenzhen.js
@@ -0,0 +1,119 @@
+export default async function handler(req, res) {
+  res.status(200).json({
+    code: 200,
+    msg: 'success',
+    success: true,
+    data: [
+      {
+          "id": 1,
+          "title": "杰克的蛋糕",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/85911754882439250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/999220250709_%E6%9D%B0%E5%85%8B%E7%9A%84%E8%9B%8B%E7%B3%95.WAV",
+          "srt": "[{\"end_time\":\"00:00:26,300\",\"id\":906,\"start_time\":\"00:00:04,400\"},{\"end_time\":\"00:00:46,000\",\"id\":906,\"start_time\":\"00:00:40,633\"},{\"end_time\":\"00:01:39,266\",\"id\":906,\"start_time\":\"00:01:27,233\"},{\"end_time\":\"00:01:42,966\",\"id\":1035,\"start_time\":\"00:01:39,266\"},{\"end_time\":\"00:02:48,800\",\"id\":906,\"start_time\":\"00:02:17,333\"},{\"end_time\":\"00:03:05,700\",\"id\":906,\"start_time\":\"00:02:55,800\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:20:17"
+      },
+      {
+          "id": 2,
+          "title": "卖栀子花的小姑娘",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/36141754882345250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/899120250709_卖栀子花的小姑娘.WAV",
+          "srt": "[{\"end_time\":\"00:00:20,566\",\"id\":1588,\"start_time\":\"00:00:07,166\"},{\"end_time\":\"00:01:00,333\",\"id\":1588,\"start_time\":\"00:00:31,133\"},{\"end_time\":\"00:02:03,000\",\"id\":1588,\"start_time\":\"00:01:41,166\"},{\"end_time\":\"00:02:40,400\",\"id\":1588,\"start_time\":\"00:02:11,033\"},{\"end_time\":\"00:03:28,300\",\"id\":1588,\"start_time\":\"00:03:10,733\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:19:40"
+      },
+      {
+          "id": 3,
+          "title": "芒果的秘密",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/96661754882238250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/771020250709_芒果的秘密.WAV",
+          "srt": "[{\"end_time\":\"00:00:26,000\",\"id\":724,\"start_time\":\"00:00:09,066\"},{\"end_time\":\"00:00:59,433\",\"id\":724,\"start_time\":\"00:00:37,266\"},{\"end_time\":\"00:01:30,266\",\"id\":724,\"start_time\":\"00:01:13,566\"},{\"end_time\":\"00:01:56,833\",\"id\":724,\"start_time\":\"00:01:35,766\"},{\"end_time\":\"00:02:54,666\",\"id\":724,\"start_time\":\"00:02:29,966\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:18:43"
+      },
+      {
+          "id": 4,
+          "title": "玫瑰花的秘密",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/54741754882489250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/967320250709_玫瑰花的秘密.WAV",
+          "srt": "[{\"end_time\":\"00:00:24,600\",\"id\":1666,\"start_time\":\"00:00:04,833\"},{\"end_time\":\"00:01:05,266\",\"id\":1666,\"start_time\":\"00:00:31,633\"},{\"end_time\":\"00:02:30,400\",\"id\":1666,\"start_time\":\"00:02:10,633\"},{\"end_time\":\"00:03:15,700\",\"id\":1666,\"start_time\":\"00:02:44,566\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:18:06"
+      },
+      {
+          "id": 5,
+          "title": "小雨的魔法芒果",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/96661754882238250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/210920250709_小雨的魔法芒果.MP3",
+          "srt": "[{\"end_time\":\"00:00:32,934\",\"id\":724,\"start_time\":\"00:00:05,534\"},{\"end_time\":\"00:01:05,524\",\"id\":724,\"start_time\":\"00:00:39,299\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:17:12"
+      },
+      {
+          "id": 6,
+          "title": "小鸟和葡萄园",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/89891754882574250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/378320250709_小鸟和葡萄园.WAV",
+          "srt": "[{\"end_time\":\"00:00:28,766\",\"id\":824,\"start_time\":\"00:00:08,266\"},{\"end_time\":\"00:01:07,933\",\"id\":824,\"start_time\":\"00:00:47,433\"},{\"end_time\":\"00:02:45,000\",\"id\":824,\"start_time\":\"00:02:25,400\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:16:42"
+      },
+      {
+          "id": 7,
+          "title": "小老鼠和爆米花",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/91131754882618250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/757020250709_小老鼠和爆米花.WAV",
+          "srt": "[{\"end_time\":\"00:00:22,100\",\"id\":946,\"start_time\":\"00:00:06,866\"},{\"end_time\":\"00:02:24,233\",\"id\":946,\"start_time\":\"00:01:51,633\"},{\"end_time\":\"00:02:44,466\",\"id\":946,\"start_time\":\"00:02:32,233\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:16:11"
+      },
+      {
+          "id": 8,
+          "title": "小浣熊吃菠萝",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/76861754882656250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250709/612120250709_小浣熊吃菠萝.WAV",
+          "srt": "[{\"end_time\":\"00:00:22,233\",\"id\":497,\"start_time\":\"00:00:01,566\"},{\"end_time\":\"00:01:07,500\",\"id\":1585,\"start_time\":\"00:00:22,533\"},{\"end_time\":\"00:02:28,633\",\"id\":1585,\"start_time\":\"00:02:02,700\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-07-09 17:15:42"
+      },
+      {
+          "id": 9,
+          "title": "比利的紫色魔法",
+          "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250811/80101754882694250811.png",
+          "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250317/318520250317_比利的紫色魔法.MP3",
+          "srt": "[{\"end_time\":\"00:00:24,732\",\"id\":1663,\"start_time\":\"00:00:05,534\"},{\"end_time\":\"00:01:16,531\",\"id\":1663,\"start_time\":\"00:00:43,080\"}]",
+          "year": 2025,
+          "type": "audio",
+          "is_show": "1",
+          "created_at": "2025-03-17 17:43:14"
+      },
+      {
+        "id": 10,
+        "title": "环游世界做苹果派",
+        "img_url": "https://res.xiaobo.qiweiwangguo.com/images/20250922/91591758513472250922.png",
+        "video_url": "https://res.xiaobo.qiweiwangguo.com/audios/20250922/776520250922_环游世界做苹果派录音1（邓媛姿）.mp3",
+        "srt": "[{\"end_time\":\"00:00:26,300\",\"id\":906,\"start_time\":\"00:00:04,400\"},{\"end_time\":\"00:00:46,000\",\"id\":906,\"start_time\":\"00:00:40,633\"},{\"end_time\":\"00:01:39,266\",\"id\":906,\"start_time\":\"00:01:27,233\"},{\"end_time\":\"00:01:42,966\",\"id\":1035,\"start_time\":\"00:01:39,266\"},{\"end_time\":\"00:02:48,800\",\"id\":906,\"start_time\":\"00:02:17,333\"},{\"end_time\":\"00:03:05,700\",\"id\":906,\"start_time\":\"00:02:55,800\"}]",
+        "year": 2025,
+        "type": "audio",
+        "is_show": "1",
+        "created_at": "2025-07-09 17:20:17"
+      },
+    ],
+  });
+}
