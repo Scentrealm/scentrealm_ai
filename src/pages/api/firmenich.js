fa6157064db72d812820ec5df765c595ddfb44e0
limeng <13020102221@163.com>
2026-05-28 14:48:36 +0800

    update device list

--- src/pages/api/firmenich.js
+++ src/pages/api/firmenich.js
@@ -3,12 +3,6 @@ export default async function handler(req, res) {
     code: 200,
     msg: 'success',
     success: true,
-    page: {
-      n: 2000,
-      total: 34,
-      last_page: 1,
-      current_page: 1,
-    },
     data: [
       '08:D1:F9:12:E8:4E',
     ]
