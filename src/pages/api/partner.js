6f69480fd9658230a8b17b3c519f4614aaefdaac
limeng <13020102221@163.com>
2026-07-07 09:20:49 +0800

    update device list

--- src/pages/api/partner.js
+++ src/pages/api/partner.js
@@ -9,224 +9,225 @@ export default async function handler(req, res) {
       last_page: 1,
       current_page: 1,
     },
-    data: [
-      {
-        "no": 345,
-        "name": "印记密码",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 123000
-      },
-      {
-        "no": 348,
-        "name": "大王来巡山",
-        "img": "/scents/xiaoshanpo.png",
-        "color": "#A9DAD7",
-        "period": 123000
-      },
-      {
-        "no": 561,
-        "name": "摇曳纯净",
-        "img": "/scents/lanfengling.png",
-        "color": "#A9DAD7",
-        "period": 87000
-      },
-      {
-        "no": 655,
-        "name": "加州阳光",
-        "img": "/scents/chenlu.png",
-        "color": "#A9DAD7",
-        "period": 87000
-      },
-      {
-        "no": 665,
-        "name": "精神满满",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 126000
-      },
-      {
-        "no": 700,
-        "name": "枕边蜜语",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 105000
-      },
-      {
-        "no": 702,
-        "name": "心动一夏",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 120000
-      },
-      {
-        "no": 1529,
-        "name": "醉梦清欢",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 180000
-      },
-      {
-        "no": 1530,
-        "name": "茉香旅途",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 225000
-      },
-      {
-        "no": 1531,
-        "name": "薰衣仲夜",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 180000
-      },
-      {
-        "no": 1532,
-        "name": "小曹寻味",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 120000
-      },
-      {
-        "no": 1533,
-        "name": "晨曦薄荷",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 180000
-      },
-      {
-        "no": 1534,
-        "name": "西湖桂花",
-        "img": "",
-        "color": "#A9DAD7",
-        "period": 225000
-      },
-      {
-        "no": 231,
-        "name": "太阳的爱",
-        "img": "/scents/chenlu.png",
-        "period": 87000
-      },
-      {
-        "no": 876,
-        "name": "蒜蓉小龙虾",
-        "img": "",
-        "period": 60000
-      },
-      {
-        "no": 232,
-        "name": "百万美元",
-        "img": "",
-        "period": 123000
-      },
-      {
-        "no": 700,
-        "name": "枕边蜜语",
-        "img": "/scents/miju.png",
-        "period": 105000
-      },
-      {
-        "no": 702,
-        "name": "心动一夏",
-        "img": "/scents/marry me.png",
-        "period": 120000
-      },
-      {
-        "no": 665,
-        "name": "精神满满",
-        "img": "/scents/bohe.png",
-        "period": 126000
-      },
-      {
-        "no": 1819,
-        "name": "桂在有你",
-        "img": "",
-        "period": 288000
-      },
-      {
-        "no": 1820,
-        "name": "无人区玫瑰",
-        "img": "",
-        "period": 120000
-      },
-      {
-        "no": 747,
-        "name": "小雏菊",
-        "img": "",
-        "period": 87000
-      },
-      {
-        "no": 1821,
-        "name": "佛曰无所谓",
-        "img": "",
-        "period": 99000
-      },
-      {
-        "no": 34,
-        "name": "檀香",
-        "img": "/scents/tanxiang.png",
-        "period": 120000
-      },
-      {
-        "no": 577,
-        "name": "沉香",
-        "img": "/scents/chenxiang.png",
-        "period": 135000
-      },
-      {
-        "no": 1822,
-        "name": "Cool Water",
-        "img": "",
-        "period": 288000
-      },
-      {
-        "no": 1823,
-        "name": "柠檬与鼠尾",
-        "img": "",
-        "period": 453600
-      },
-      {
-        "no": 762,
-        "name": "小山坡",
-        "img": "",
-        "period": 123000
-      },
-      {
-        "no": 1824,
-        "name": "浆果汽水儿",
-        "img": "",
-        "period": 360000
-      },
-      {
-        "no": 191,
-        "name": "大地",
-        "img": "",
-        "period": 123000
-      },
-      {
-        "no": 56,
-        "name": "海洋",
-        "img": "",
-        "period": 123000
-      },
-      {
-        "no": 5,
-        "name": "森林",
-        "img": "/scents/senlin.png",
-        "period": 105000
-      },
-      {
-        "no": 1826,
-        "name": "心想事橙",
-        "img": "",
-        "period": 378000
-      },
-      {
-        "no": 1825,
-        "name": "麻辣鲜香",
-        "img": "",
-        "period": 60000
-      }
-    ]
+    data: [],
+    // data: [
+    //   {
+    //     "no": 345,
+    //     "name": "印记密码",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 123000
+    //   },
+    //   {
+    //     "no": 348,
+    //     "name": "大王来巡山",
+    //     "img": "/scents/xiaoshanpo.png",
+    //     "color": "#A9DAD7",
+    //     "period": 123000
+    //   },
+    //   {
+    //     "no": 561,
+    //     "name": "摇曳纯净",
+    //     "img": "/scents/lanfengling.png",
+    //     "color": "#A9DAD7",
+    //     "period": 87000
+    //   },
+    //   {
+    //     "no": 655,
+    //     "name": "加州阳光",
+    //     "img": "/scents/chenlu.png",
+    //     "color": "#A9DAD7",
+    //     "period": 87000
+    //   },
+    //   {
+    //     "no": 665,
+    //     "name": "精神满满",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 126000
+    //   },
+    //   {
+    //     "no": 700,
+    //     "name": "枕边蜜语",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 105000
+    //   },
+    //   {
+    //     "no": 702,
+    //     "name": "心动一夏",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 120000
+    //   },
+    //   {
+    //     "no": 1529,
+    //     "name": "醉梦清欢",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 180000
+    //   },
+    //   {
+    //     "no": 1530,
+    //     "name": "茉香旅途",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 225000
+    //   },
+    //   {
+    //     "no": 1531,
+    //     "name": "薰衣仲夜",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 180000
+    //   },
+    //   {
+    //     "no": 1532,
+    //     "name": "小曹寻味",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 120000
+    //   },
+    //   {
+    //     "no": 1533,
+    //     "name": "晨曦薄荷",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 180000
+    //   },
+    //   {
+    //     "no": 1534,
+    //     "name": "西湖桂花",
+    //     "img": "",
+    //     "color": "#A9DAD7",
+    //     "period": 225000
+    //   },
+    //   {
+    //     "no": 231,
+    //     "name": "太阳的爱",
+    //     "img": "/scents/chenlu.png",
+    //     "period": 87000
+    //   },
+    //   {
+    //     "no": 876,
+    //     "name": "蒜蓉小龙虾",
+    //     "img": "",
+    //     "period": 60000
+    //   },
+    //   {
+    //     "no": 232,
+    //     "name": "百万美元",
+    //     "img": "",
+    //     "period": 123000
+    //   },
+    //   {
+    //     "no": 700,
+    //     "name": "枕边蜜语",
+    //     "img": "/scents/miju.png",
+    //     "period": 105000
+    //   },
+    //   {
+    //     "no": 702,
+    //     "name": "心动一夏",
+    //     "img": "/scents/marry me.png",
+    //     "period": 120000
+    //   },
+    //   {
+    //     "no": 665,
+    //     "name": "精神满满",
+    //     "img": "/scents/bohe.png",
+    //     "period": 126000
+    //   },
+    //   {
+    //     "no": 1819,
+    //     "name": "桂在有你",
+    //     "img": "",
+    //     "period": 288000
+    //   },
+    //   {
+    //     "no": 1820,
+    //     "name": "无人区玫瑰",
+    //     "img": "",
+    //     "period": 120000
+    //   },
+    //   {
+    //     "no": 747,
+    //     "name": "小雏菊",
+    //     "img": "",
+    //     "period": 87000
+    //   },
+    //   {
+    //     "no": 1821,
+    //     "name": "佛曰无所谓",
+    //     "img": "",
+    //     "period": 99000
+    //   },
+    //   {
+    //     "no": 34,
+    //     "name": "檀香",
+    //     "img": "/scents/tanxiang.png",
+    //     "period": 120000
+    //   },
+    //   {
+    //     "no": 577,
+    //     "name": "沉香",
+    //     "img": "/scents/chenxiang.png",
+    //     "period": 135000
+    //   },
+    //   {
+    //     "no": 1822,
+    //     "name": "Cool Water",
+    //     "img": "",
+    //     "period": 288000
+    //   },
+    //   {
+    //     "no": 1823,
+    //     "name": "柠檬与鼠尾",
+    //     "img": "",
+    //     "period": 453600
+    //   },
+    //   {
+    //     "no": 762,
+    //     "name": "小山坡",
+    //     "img": "",
+    //     "period": 123000
+    //   },
+    //   {
+    //     "no": 1824,
+    //     "name": "浆果汽水儿",
+    //     "img": "",
+    //     "period": 360000
+    //   },
+    //   {
+    //     "no": 191,
+    //     "name": "大地",
+    //     "img": "",
+    //     "period": 123000
+    //   },
+    //   {
+    //     "no": 56,
+    //     "name": "海洋",
+    //     "img": "",
+    //     "period": 123000
+    //   },
+    //   {
+    //     "no": 5,
+    //     "name": "森林",
+    //     "img": "/scents/senlin.png",
+    //     "period": 105000
+    //   },
+    //   {
+    //     "no": 1826,
+    //     "name": "心想事橙",
+    //     "img": "",
+    //     "period": 378000
+    //   },
+    //   {
+    //     "no": 1825,
+    //     "name": "麻辣鲜香",
+    //     "img": "",
+    //     "period": 60000
+    //   }
+    // ]
   })
 }
